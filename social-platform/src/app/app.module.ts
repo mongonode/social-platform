4162c18ecb1ca882a11d310a81a370f5f7b6f7f5
Chamith Hirushan <chamith.ictech@gmail.com>
2023-02-07 00:18:20 +0530

    New 12.18

--- social-platform/src/app/app.module.ts
+++ social-platform/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Layout/header/header.component';
+import { FooterComponent } from './Components/Layout/footer/footer.component';
+import { HomePageComponent } from './Components/Home/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
